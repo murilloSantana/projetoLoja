344a3d16b9cb0bc786b2d01e277f8ca8529a8ade
murillo <murillo.costa.ferreira@gmail.com>
2015-07-13 15:58:36 -0300

    excluir
    excuir primeira versão

--- js/app.js
+++
@@ -1,1 +0,0 @@
-var app = angular.module("app",['ngRoute','ui.bootstrap','angularUtils.directives.dirPagination']);
--- js/diretivas/menu.js
+++
@@ -1,6 +0,0 @@
-app.directive('menu', function () {
-	return {
-		restrict: 'AE',
-		templateUrl:'/ProjetoLoja/views/menu.html'
-	};
-});
